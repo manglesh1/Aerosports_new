68c3611495d90f4ce204535da8d430bac95f3111
yogesh <yogeshniranjan786@gmail.com>
2024-09-11 18:45:16 +0530

    Image: add header image on subcategory pages

--- src/app/[location_slug]/[category_slug]/[subcategory_slug]/page.jsx
+++ src/app/[location_slug]/[category_slug]/[subcategory_slug]/page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "../../../styles/subcategory.css";
+import "../../../styles/kidsparty.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { getDataByParentId } from "@/utils/customFunctions";
 import { fetchData } from "@/utils/fetchData";
+import MotionImage from "@/components/MotionImage";
 
 export async function generateMetadata({ params }) {
   const { location_slug, subcategory_slug } = params;
@@ -32,6 +34,7 @@ const Subcategory = async ({ params }) => {
   const { location_slug, subcategory_slug } = params;
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+ 
   const data = await fetchData(
     `${API_URL}/fetchsheetdata?sheetname=Data&location=${location_slug}`
   );
@@ -41,12 +44,16 @@ const Subcategory = async ({ params }) => {
   );
 
   const booknow = dataconfig?.filter((item) => item.key === "estorebase");
-
+  const waiver = dataconfig?.filter((item) => item.key === "waiver");
   const attractionsData = getDataByParentId(data, subcategory_slug);
-
+  const header_image = getDataByParentId(data, subcategory_slug);
+ 
   return (
     <main>
       <Header location_slug={location_slug} booknow={booknow} />
+      <section>
+        <MotionImage header_image={header_image} waiver={waiver} />
+      </section>
       <section className="aero-max-container">
         <div
           className="subcategory_main_section"
