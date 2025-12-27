37816d6b07045cb17fd005af184b6257559cafc9
Rajan Ghimire <rajanghimire534@gmail.com>
2025-12-27 00:04:36 -0500

    overwrite the static sitemap generator

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,14 @@
+/**
+ * DEPRECATED - This configuration is no longer used.
+ *
+ * The sitemap is now generated dynamically via the route handler at:
+ * src/app/sitemap.xml/route.js
+ *
+ * This provides real-time updates from Google Sheets without requiring rebuilds.
+ *
+ * The postbuild script has been removed from package.json to prevent conflicts.
+ */
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://www.aerosportsparks.ca',
